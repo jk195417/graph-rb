7cfbe0dbe31dbcab333fd76fcc92953c1f26d80b
楊竑昕 <jk195417@gmail.com>
2019-04-26 00:42:28 +0800

    Initial commit

---
+++ spec/graph_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Graph do
+  it "has a version number" do
+    expect(Graph::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
